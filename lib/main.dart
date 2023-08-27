8ae3277170d391741659b2960e8ea8d45c0daf92
Badizawr <vicodin-13@ya.ru>
2023-08-27 23:33:56 +0500

    create demo progect

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CryptoCurrenciesListApp());
+}
+
+class CryptoCurrenciesListApp extends StatelessWidget {
+  const CryptoCurrenciesListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CryptoCurrenciesList',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(255, 179, 204, 69),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 110, 196, 12)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Crypto Currencies List'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
